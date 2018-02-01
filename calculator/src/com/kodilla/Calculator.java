ac0944e228c79a9350cc019947a49efbe145f958
Anna Czerska <anna.czerska@azetti.com>
2018-02-01 16:48:28 +0100

    Divide calculations method into 2 separate methods

--- calculator/src/com/kodilla/Calculator.java
+++ calculator/src/com/kodilla/Calculator.java
@@ -2,19 +2,23 @@ package com.kodilla;
 
 public class Calculator {
 
-    public void calculations(int a, int b) {
-        int c = a+b;
-        System.out.println("SUM = " + c);
+    public void substraction(int a, int b) {
         int d = a-b;
         System.out.println("DIFF = " + d);
-        System.out.println("There you go! Calculated :)");
+        System.out.println("There you go! Difference is calculated :)");
     }
 
+    public void addition(int a, int b) {
+        int c = a+b;
+        System.out.println("SUM = " + c);
+        System.out.println("There you go! Sum is calculated :)");
+    }
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-        calculator.calculations(10,5);
+        calculator.substraction(10,5);
+        calculator.addition(10,5);
 
     }
 }
