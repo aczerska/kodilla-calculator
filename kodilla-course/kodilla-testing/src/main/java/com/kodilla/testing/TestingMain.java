f9b903da9cec8b1b1ac5d3b96137382be0330fc1
Anna Czerska <anna.czerska@azetti.com>
2018-02-05 17:14:50 +0100

    Add first Unit Test for SimpleUser and Calculator

--- kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("\nFirst Unit Test");
+
+        Calculator calculator = new Calculator();
+
+        int a = 10;
+        int b = 5;
+
+        if(calculator.add(a,b) == a+b) {
+            System.out.println("Test passed for add method");
+        } else {
+            System.out.println("Test failed for add method");
+        }
+
+        if(calculator.subtract(a,b) == a-b) {
+            System.out.println("Test passed for subtract method");
+        } else {
+            System.out.println("Test failed for subtract method");
+        }
     }
 }
