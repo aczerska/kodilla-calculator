547cab9865bab8ede2a848e7f600e6bb01db2c9d
Anna Czerska <ania.czerska@gmail.com>
2018-02-08 11:25:17 +0100

    Add first JUnit test

--- kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,33 +5,6 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if(result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        System.out.println("\nFirst Unit Test");
-
-        Calculator calculator = new Calculator();
-
-        int a = 10;
-        int b = 5;
-
-        if(calculator.add(a,b) == a+b) {
-            System.out.println("Test passed for add method");
-        } else {
-            System.out.println("Test failed for add method");
-        }
-
-        if(calculator.subtract(a,b) == a-b) {
-            System.out.println("Test passed for subtract method");
-        } else {
-            System.out.println("Test failed for subtract method");
-        }
     }
 }
