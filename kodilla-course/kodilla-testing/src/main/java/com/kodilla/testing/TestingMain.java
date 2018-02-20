19e6bd2688b8f855edfcaded8328baa3043deeee
Anna Czerska <ania.czerska@gmail.com>
2018-02-20 17:12:00 +0100

    Add tests to TestingMain (task #6.2)

--- kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,6 +5,34 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
+        // Testing SimpleUser
+        System.out.println("\nTesting SimpleUser class:");
+        SimpleUser simpleUser = new SimpleUser("JohnnyBravo", "John John");
 
+        String result = simpleUser.getUsername();
+
+        if (result.equals("JohnnyBravo")){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // Testing Calculator
+        System.out.println("\nTesting Calculator class:");
+        Calculator calculator = new Calculator();
+        int resultSub = calculator.subtract(10,5);
+        int resultAdd = calculator.add(5,5);
+
+        if(resultSub == 5) {
+            System.out.println("Add method works!");
+        } else {
+            System.out.println("Add method failed!");
+        }
+
+        if(resultAdd == 10) {
+            System.out.println("Subtract method works!");
+        } else {
+            System.out.println("Subtract method failed!");
+        }
     }
 }
