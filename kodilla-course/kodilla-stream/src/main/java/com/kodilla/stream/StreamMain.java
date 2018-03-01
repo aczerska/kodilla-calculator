9b0963e6be6a14be2c461622460f67ee202c2274
Anna Czerska <ania.czerska@gmail.com>
2018-03-01 17:00:41 +0100

    Example with lambda expression (#7.1)

--- kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
     public static void main(String[] args) {
         Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
+        processor.execute(() -> System.out.println("This is an example text."));
     }
 }
