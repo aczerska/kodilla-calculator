a370c98fa6e3c15f4c13bb0bec44014085a90fc2
Anna Czerska <ania.czerska@gmail.com>
2018-03-07 12:42:10 +0100

    Stream flow (#7.3)

--- kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,37 +1,13 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        System.out.println("\nBeautification task #7.1");
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("TEXT",(text -> "abc" + text + "abc"));
-        poemBeautifier.beautify("TEXT",(text -> text.replace('T', 't')));
-        poemBeautifier.beautify("poem",(text -> "ABC" + text.concat("ABC")));
-        poemBeautifier.beautify("poem", String::toUpperCase);
-        poemBeautifier.beautify("This was a beautiful day",(text -> text.substring(11,24).replace('a', 'A')));
-
-        System.out.println("\nSTREAM");
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        People.getList().stream()
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
     }
 }
