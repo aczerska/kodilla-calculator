8e8158b421b8086398134cec9db0aceccf2dd62d
Anna Czerska <ania.czerska@gmail.com>
2018-03-15 12:02:41 +0100

    Solution PeopleQuantity final (task #7.4)

--- kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-course/kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -3,49 +3,19 @@ package com.kodilla.stream.world;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public final class World {
-    private final Continent continent;
-    private final List<Continent> continentList = new ArrayList<>();
 
-    public World(final Continent continent) {
-        this.continent = continent;
-    }
+    private final List<Continent> continentList = new ArrayList<>();
 
     public void addContinent(Continent continent) {
         continentList.add(continent);
-    }
-
-    public Continent getContinent() {
-        return continent;
     }
 
     public List<Continent> getContinentList() {
         return continentList;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        World world = (World) o;
-        return Objects.equals(continent, world.continent);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(continent);
-    }
-
-    @Override
-    public String toString() {
-        return "World{" +
-                "continent=" + continent +
-                ", continentList=" + continentList +
-                '}';
-    }
     public BigDecimal getPeopleQuantity() {
         return continentList.stream()
                 .flatMap(continent -> continent.getCountryList().stream())
--- kodilla-course/kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-course/kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -1,11 +1,48 @@
 package com.kodilla.stream.world;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldTestSuite {
 
     @Test
     public void testGetPeopleQuantity() {
+        // Given
+        World world = new World();
 
+        Continent asia = new Continent();
+        Continent europe = new Continent();
+        Continent africa = new Continent();
+
+        List<Continent> continentList = new ArrayList<>();
+        world.addContinent(asia);
+        world.addContinent(africa);
+        world.addContinent(europe);
+
+        Country poland = new Country();
+        Country spain = new Country();
+        Country japan = new Country();
+        Country korea = new Country();
+        Country nigeria = new Country();
+        Country kongo = new Country();
+
+        List<Country> countryList = new ArrayList<>();
+        asia.addCountry(japan);
+        asia.addCountry(korea);
+        europe.addCountry(poland);
+        europe.addCountry(spain);
+        africa.addCountry(nigeria);
+        africa.addCountry(kongo);
+
+        // When
+        BigDecimal worldPopulation = world.getPeopleQuantity();
+
+        // Then
+        BigDecimal expectedWorldPopulation = new BigDecimal("863788254");
+        Assert.assertEquals(expectedWorldPopulation, worldPopulation);
     }
 }
