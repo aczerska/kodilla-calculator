0e3e093af01407619843b1c88b9131d05946c20c
Anna Czerska <ania.czerska@gmail.com>
2018-03-21 16:24:31 +0100

    Small changes in Game class (#7.7)

--- kodilla-course/kodilla-rps/src/main/java/com/kodilla/rps/Game.java
+++ kodilla-course/kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -1,10 +1,25 @@
 package com.kodilla.rps;
 
-public class Game {
- private boolean end = false;
-    public void run() {
-        while(!end){
+import java.util.Scanner;
 
-        }
+public class Game {
+
+    private String username;
+    private int rounds;
+
+    public Game(String username, int rounds) {
+        this.username = username;
+        this.rounds = rounds;
+    }
+
+    public void getInfo() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        username = in.next();
+
+        System.out.println("Enter number of rounds you want to play:");
+        rounds = in.nextInt();
     }
 }
+
