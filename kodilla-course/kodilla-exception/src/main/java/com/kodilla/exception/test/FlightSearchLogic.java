e27fcee5bc390dd6bb3645096518710f635a119a
Anna Czerska <ania.czerska@gmail.com>
2018-03-23 15:01:14 +0100

    Add helper method (task #8.4)

--- kodilla-course/kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearchLogic.java
+++ kodilla-course/kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearchLogic.java
@@ -4,15 +4,32 @@ import java.util.*;
 
 public class FlightSearchLogic {
 
-    public void findFlight(Flight flight) throws RouteNotFoundException {
-        Map<String, Boolean> flightMap = new HashMap<>();
+    Map<String, Boolean> flightMap = new HashMap<>();
+
+    public Map<String, Boolean> populateMap(Map<String,Boolean> map) {
         flightMap.put("Amsterdam", true);
         flightMap.put("Warsaw", true);
         flightMap.put("London", false);
         flightMap.put("Madrid", false);
+        return flightMap;
+    }
 
-        if (flightMap.containsKey(flight.getArrivalAirport()) && flightMap.get(flight.getArrivalAirport())
-                && flightMap.containsKey(flight.getDepartureAirport()) && flightMap.get(flight.getDepartureAirport())) {
+    public boolean checkAirport(Map<String,Boolean> map, Flight flight) {
+        boolean operate;
+        if (flightMap.containsKey(flight.getArrivalAirport()) &&
+                flightMap.get(flight.getArrivalAirport()) &&
+                flightMap.containsKey(flight.getDepartureAirport()) &&
+                flightMap.get(flight.getDepartureAirport())) {
+            operate = true;
+        } else {
+            operate = false;
+        }
+        return operate;
+    }
+
+    public void findFlight(boolean operate) throws RouteNotFoundException {
+
+        if (operate) {
             System.out.println("Chosen route is available, you can proceed with reservation procedures");
         } else {
             throw new RouteNotFoundException("Arrival and/or departure airport do not exist in data base.");
