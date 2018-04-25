3040966f8339d51ff95c39b22d8f2917e23412f5
Anna Czerska <ania.czerska@gmail.com>
2018-04-25 17:30:53 +0200

    Fix package (#11.1)

--- kodilla-course/kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/SettingsFileEngine.java
+++ kodilla-course/kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/SettingsFileEngine.java
@@ -1,4 +1,4 @@
-package singleton;
+package com.kodilla.patterns.singleton;
 
 public class SettingsFileEngine {
     private static SettingsFileEngine settingsFileEngineInstance = null;
--- kodilla-course/kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileEngineTestSuite.java
+++ kodilla-course/kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileEngineTestSuite.java
@@ -4,7 +4,6 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import singleton.SettingsFileEngine;
 
 public class SettingsFileEngineTestSuite {
     @BeforeClass
