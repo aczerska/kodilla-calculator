5ffb039c4dfe66e623de8cce23f6a0cf45bc2dcf
Anna Czerska <ania.czerska@gmail.com>
2018-04-26 16:13:33 +0200

    Prototype Library - deep copy (task #11.2)

--- kodilla-course/kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-course/kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -3,9 +3,9 @@ package com.kodilla.patterns.prototype.library;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Library {
-    final String name;
-    final Set<Book> books = new HashSet<>();
+public class Library extends Prototype {
+    private String name;
+    private Set<Book> books = new HashSet<>();
 
     public Library(final String name) {
         this.name = name;
@@ -13,6 +13,10 @@ public class Library {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Set<Book> getBooks() {
@@ -26,5 +30,14 @@ public class Library {
             s = s + book.toString() + "\n";
         }
         return s;
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library) super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books) {
+            clonedLibrary.getBooks().add(book);
+        }
+        return clonedLibrary;
     }
 }
--- kodilla-course/kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-course/kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.patterns.prototype.library;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -9,15 +10,30 @@ public class LibraryTestSuite {
     public void testGetBooks() {
         //Given
         Library library = new Library("Library 1");
-        Book book1 = new Book("Title 1","Author 1", LocalDate.of(2000, 10, 01));
-        Book book2 = new Book("Title 2","Author 2", LocalDate.of(1999, 01, 20));
-        Book book3 = new Book("Title 3","Author 3", LocalDate.of(1945, 12, 24));
-        library.getBooks().add(book1);
-        library.getBooks().add(book2);
-        library.getBooks().add(book3);
-        //Then
+        library.getBooks().add(new Book("Title 1","Author 1", LocalDate.of(2000, 10, 01)));
+        library.getBooks().add(new Book("Title 2","Author 2", LocalDate.of(1999, 01, 20)));
+        library.getBooks().add(new Book("Title 3","Author 3", LocalDate.of(1945, 12, 24)));
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
         System.out.println(library);
+        System.out.println(deepClonedLibrary);
+
         //When
+        deepClonedLibrary.getBooks().add(new Book("Title 4","Author 4", LocalDate.of(1991, 01, 01)));
+
+        //Then
+        Assert.assertEquals(3, library.getBooks().size());
+        Assert.assertEquals(4, deepClonedLibrary.getBooks().size());
+
+        System.out.println(library);
+        System.out.println(deepClonedLibrary);
 
     }
 
