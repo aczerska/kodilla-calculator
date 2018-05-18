1ecd9dd073735b98f1e955763146205ae7c2b0e9
Anna Czerska <ania.czerska@gmail.com>
2018-05-18 14:48:08 +0200

    Example relations between tables 1:N (#13.3)

--- kodilla-course/kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-course/kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,6 +1,10 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -8,6 +12,7 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -44,6 +49,20 @@ public class TaskList {
 
     private void setDescription(String description) {
         this.description = description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
